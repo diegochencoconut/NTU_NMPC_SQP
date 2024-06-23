e203c0e4a313df35b8f08eb616e40bb54aca777a
COCONUT <b10901061@ntu.edu.tw>
2024-06-23 15:20:44 +0800

    Nonlinear UAV init

--- main.m
+++ main.m
@@ -7,13 +7,13 @@ real_Ts = 0.01;
 N = 10;
 simTime = 5;
 Time = 0:Ts:simTime;
-options =  optimset('Display','off');
-MAXIMUM_LOOP = 30;
+options = optimoptions('quadprog','Algorithm','active-set','Display','final');
+MAXIMUM_LOOP = 10;
 
 %% Initialize
 state_size = 6;         % Check consistent with model.m
 input_size = 2;         % Check consistent with model.m
-init_state = [1.9,5.9,-0.538,-0.548,-0.2,-0.2]';   % Check consistent
+init_state = [1.9,5.9,-0.538,-0.548,0,0]';   % Check consistent
 
 best_state = zeros(state_size, length(Time));
 best_input = zeros(input_size, length(Time));
@@ -42,14 +42,16 @@ for timeTick = 1:(length(Time)-1)
         max_input = 4*ones(input_size*N, 1);
         min_input = -4*ones(input_size*N, 1);
         
-        % max_state = reshape([Inf 0;0 Inf] * ones(state_size, N), [], 1);
-        % min_state = reshape([-Inf 0;0 -Inf]' * ones(state_size, N), [], 1);
+        max_state = diag([3.5, 7.5,2,2,0.2,0.2]);
+        min_state = diag([-10, -4.5,-2,-2,-0.2,-0.2]);
+        max_state = reshape(max_state * ones(state_size, N), [], 1);
+        min_state = reshape(min_state * ones(state_size, N), [], 1);
 
         % Find best direction
         oneline_input = reshape(input, [], 1);
         oneline_state = reshape(state, [], 1);
         % du = quadprog(H, grad_cost',[eye(input_size*N); -eye(input_size*N); grad_state'; -grad_state'],[max_input-oneline_input; oneline_input-min_input; max_state-oneline_state ; oneline_state-min_state],[],[],[],[],input,options);
-        du = quadprog(H, grad_cost',[eye(input_size*N); -eye(input_size*N)],[max_input-oneline_input; oneline_input-min_input],[],[],[],[],input,options);
+        du = quadprog(H, grad_cost',[eye(input_size*N); -eye(input_size*N); grad_state'; -grad_state'],[max_input-oneline_input; oneline_input-min_input; max_state-oneline_state ; oneline_state-min_state],[],[],[],[],input,options);
         
         % update value
         input = input + reshape(du, input_size, N);
@@ -78,7 +80,9 @@ for timeTick = 1:(length(Time)-1)
 
 
     end
-    loop
+    if (loop == MAXIMUM_LOOP)
+        disp("MAXIMUM LOOP REACHED");
+    end
 
     % real model (with faster Ts)
     best_input(:,timeTick) = input(:, 1);
@@ -88,14 +92,37 @@ for timeTick = 1:(length(Time)-1)
     % best_state(:,timeTick) = state(:, 1);
     start_state = real_plant_state(:, end);
 
+    % calculate the init of next iteration
     input = [input(:, 2:end), input(:, end)];
     state = model(start_state, Ts, input, N);
 
     [grad_cost, grad_state] = find_gradient(input, delta, start_state, input_size, state_size, Ts, Q, R, N);
 end
 %% Plot results
+best_state = [init_state best_state(:,1:end-1)];
+best_input = [best_input(:,1:end-1), best_input(:,end-1)];
 figure;
-subplot(2, 1, 1);
-plot(Time, [init_state best_state(:,1:end-1)])
-subplot(2, 1, 2)
-plot(Time, [best_input(:,1:end-1), best_input(:,end-1)])
+subplot(4, 2, 1);
+plot(Time, best_state(1, :));
+ylabel('$x$', 'Interpreter','latex');
+subplot(4, 2, 2);
+plot(Time, best_state(2, :));
+ylabel('$y$', 'Interpreter','latex');
+subplot(4, 2, 3);
+plot(Time, best_state(3, :));
+ylabel('$\dot{x}$', 'Interpreter','latex');
+subplot(4, 2, 4);
+plot(Time, best_state(4, :));
+ylabel('$\dot{y}$', 'Interpreter','latex');
+subplot(4, 2, 5);
+plot(Time, best_state(5, :));
+ylabel('$\theta$', 'Interpreter','latex');
+subplot(4, 2, 6);
+plot(Time, best_state(6, :));
+ylabel('$\psi$', 'Interpreter','latex');
+subplot(4, 2, 7);
+plot(Time, best_input(1, :));
+ylabel('$\dot{\theta}$', 'Interpreter','latex');
+subplot(4, 2, 8);
+plot(Time, best_input(2, :));
+ylabel('$\dot{\psi}$', 'Interpreter','latex');
--- model.m
+++ model.m
@@ -16,33 +16,33 @@ end
 
 
 function next_state = model_equation(current_state, Ts, input)
-    g = 9.8;
-    kdm = 1;
-    A_CT = [0 0 1 0 0 0;
-            0 0 0 1 0 0;
-            0 0 -kdm 0 g 0;
-            0 0 0 -kdm 0 -g;
-            0 0 0 0 0 0;
-            0 0 0 0 0 0];
-    B_CT = [0 0;
-            0 0;
-            0 0;
-            0 0;
-            1 0;
-            0 1];
-    next_state = current_state + Ts * (A_CT * current_state + B_CT * input);
-
-    % kd = 1;
-    % m = 1;
     % g = 9.8;
+    % kdm = 1;
+    % A_CT = [0 0 1 0 0 0;
+    %         0 0 0 1 0 0;
+    %         0 0 -kdm 0 g 0;
+    %         0 0 0 -kdm 0 -g;
+    %         0 0 0 0 0 0;
+    %         0 0 0 0 0 0];
+    % B_CT = [0 0;
+    %         0 0;
+    %         0 0;
+    %         0 0;
+    %         1 0;
+    %         0 1];
+    % next_state = current_state + Ts * (A_CT * current_state + B_CT * input);
 
-    % next_state(1) = current_state(1) + Ts * current_state(3);
-    % next_state(2) = current_state(2) + Ts * current_state(4);
+    kd = 1;
+    m = 1;
+    g = 9.8;
+
+    next_state(1) = current_state(1) + Ts * current_state(3);
+    next_state(2) = current_state(2) + Ts * current_state(4);
     % next_state(2) = 0;
-    % next_state(3) = current_state(3) + Ts * (-kd/m*current_state(3) + g*tan(current_state(5)));
-    % next_state(4) = current_state(4) + Ts * (-kd/m*current_state(4) - g*tan(current_state(6))/cos(current_state(5)));
+    next_state(3) = current_state(3) + Ts * (-kd/m*current_state(3) + g*tan(current_state(5)));
+    next_state(4) = current_state(4) + Ts * (-kd/m*current_state(4) - g*tan(current_state(6))/cos(current_state(5)));
     % next_state(4) = 0;
-    % next_state(5) = current_state(5) + Ts * input(1);
-    % next_state(6) = current_state(6) + Ts * input(2);
+    next_state(5) = current_state(5) + Ts * input(1);
+    next_state(6) = current_state(6) + Ts * input(2);
     % next_state(6) = 0;
 end
